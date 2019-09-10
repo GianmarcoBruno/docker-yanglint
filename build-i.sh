5fd4c53f1d9961c7b97ae33624080a5e30eaea75
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2019-09-10 20:41:58 +0200

    build-i.sh: removed explicit hosts used for building

--- build-i.sh
+++ build-i.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 VERSION="0.5"
 docker build \
-    --add-host dl-cdn.alpinelinux.org:151.101.36.249 \
-    --add-host github.com:140.82.118.3 \
     -t docker-yanglint-i:${VERSION} \
     -f Dockerfile-i  .
